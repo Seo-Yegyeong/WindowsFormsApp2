09b22253bf919b4863ce2d85bcd6766294756c68
ChoiMari <chl98639863@gmail.com>
2025-04-25 11:41:32 +0900

    마리 & 안녕하세유..

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -18,5 +18,15 @@ namespace WindowsFormsApp2
             MessageBox.Show("Welcome!");
             textBox_print.Text = "this is \r\na multiline \r\ntextbox!";
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요 호호");
+        }
+
+        private void textBox_print_TextChanged(object sender, EventArgs e)
+        {
+            textBox_print.Text = "안녕하세유...";
+        }
     }
 }
